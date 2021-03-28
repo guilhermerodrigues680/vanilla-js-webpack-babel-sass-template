32b902f875686e90fda1fe397f2ada3912f7a9e1
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-03-28 08:21:05 -0300

    Integração com Browserslist

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,6 @@ import "../assets/styles/reset.css";
 import "../assets/styles/common.scss";
 import "../assets/styles/button.scss";
 
-// https://stackoverflow.com/questions/53558916/babel-7-referenceerror-regeneratorruntime-is-not-defined
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
 document.querySelector("#btn").addEventListener('click', () => {
   const div = document.createElement('div');
   div.innerHTML = "JS OK!";
